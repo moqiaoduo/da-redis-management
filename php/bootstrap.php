862774fdb06517223d60ca980b328d337bc65901
moqiaoduo <415372363@qq.com>
2020-03-04 15:37:35 +0800

    更改iframe宽度

--- php/bootstrap.php
+++ php/bootstrap.php
@@ -8,3 +8,8 @@ parse_str(getenv('POST'), $_POST);
 require_once dirname(__DIR__) . '/php/Controllers/RedisController.php';
 ?>
 <link href="https://cdn.bootcss.com/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+<script>
+    document.getElementById("iframe-container").style.display="inline-block";
+    document.getElementById("iframe-container").style['max-width']='100vw';
+    document.getElementById("iframe-container").style.overflow='auto';
+</script>
